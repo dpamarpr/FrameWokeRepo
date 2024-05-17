2affd64636f97f7621693a96449985f76b3735a3
ACER <ACER@divyaprakashAmar>
2024-05-17 13:06:32 +0530

    newly addedrepo

---
+++ com.AmarPractice.MAven/src/test/java/Practice_Pack1/ContactTest.java
@@ -0,0 +1,34 @@
+package Practice_Pack1;
+
+/*import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver; */
+import org.testng.annotations.Test;
+
+public class ContactTest {
+		
+		@Test
+		public void creartecontactTest() {
+			/*String BROWSER=System.getProperty("browser");
+			String URL=System.getProperty("url");
+			String USERNAME=System.getProperty("username");
+			String PASSWORD=System.getProperty("password");
+			WebDriver driver = new ChromeDriver();
+			driver.get(URL);*/
+			
+			System.out.println("execute creartecontactTest");
+			System.out.println("BROWSER");
+			System.out.println("URL");
+			System.out.println("USERNAME");
+			System.out.println("PASSWORD");
+
+
+			
+		}
+		
+		@Test
+		public void midifycontactTest() {
+			
+			System.out.println("modify creartecontactTest");
+			
+		}
+}
---
+++ com.AmarPractice.MAven/src/test/java/Practice_Pack2/OrgTest.java
@@ -0,0 +1,20 @@
+package Practice_Pack2;
+
+import org.testng.annotations.Test;
+
+public class OrgTest {
+		
+		@Test
+		public void crearteOrgTest() {
+			
+			System.out.println("execute crearteOrgTest");
+			
+		}
+		
+		@Test
+		public void modifyOrgTest() {
+			
+			System.out.println("modify modifyOrgTest");
+			
+		}
+}
